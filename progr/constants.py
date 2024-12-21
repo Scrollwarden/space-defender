9331a2e1ea417a861b0316a75ceb75b5c08c6ef9
Scrollwarden <156528205+Scrollwarden@users.noreply.github.com>
2024-12-21 11:21:47 +0100

    [add] New Ennemies textures
    Changed textures for ennemies.
    Texture for player unsatifing
    Hitboxes unrespected.

--- progr/constants.py
+++ progr/constants.py
@@ -50,7 +50,7 @@ PATH_IMAGES = "../ress/img/"
 LOGO_STUDIO = PATH_IMAGES+"logo_dicescreen_pyxel.pyxres"
 LOGO_GAME = PATH_IMAGES+"logo_space_defender_pyxel.pyxres"
 SPACESHIP_TEXTURES = PATH_IMAGES+"astronefs_textures.pyxres"
-SPACESHIP_SCALE = 1.25
+SPACESHIP_SCALE = 1
 
 # colors
 
